381c3160ae1f7ac6bbe4428f16220a539cc6742c
Samuel Ribeiro <samuelaceitem4@gmail.com>
2025-02-24 17:46:10 -0300

    created tipagem-fraca

---
+++ tipagem-fraca.js
@@ -0,0 +1,17 @@
+    
+
+    let qualquer = 'legal'
+    console.log(qualquer)
+    console.log(typeof qualquer)
+
+    qualquer = 3.645
+    console.log(qualquer)
+    console.log(typeof qualquer)
+
+    //  Evitar nomes genéricos e siglas
+
+    let valor = ''
+    let numero = 5
+    let pqp = false
+
+    
