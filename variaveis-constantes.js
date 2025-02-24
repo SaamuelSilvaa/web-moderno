678cd685340c11dc31138d9cb50ff19f1d29e831
Samuel Ribeiro <samuelaceitem4@gmail.com>
2025-02-24 17:37:29 -0300

    created variaveis-constantes

---
+++ variaveis-constantes.js
@@ -0,0 +1,15 @@
+ 
+ var a = 3  // var e let sao variaveis que podem ser mudadas.
+ let b = 4
+
+ var a = 30
+ b = 40
+ console.log(a,b)
+
+ a = 300
+ b = 400
+
+console.log(a,b)
+
+const c = 5  //const não se muda o valor
+console.log(c)
